8bb55ea6caf366adbcbcff9cfe4813db2586f7e7
Mizerski <mizerskimachado@hotmail.com>
2024-04-08 20:34:15 -0300

    fix bullet

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,3 +1,9 @@
-extends CharacterBody2D
+extends Area2D
 
+@export var bullet_speed = 600
 
+func _physics_process(delta):
+	global_position += transform.x * bullet_speed * delta
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,24 +8,36 @@ extends CharacterBody2D
 
 var target_velocity = Vector2()
 var inertia = Vector2()
+var bullet_path = preload("res://Models/bullet.tscn")
 
 func get_input():
-    if health <= 0:
-        return
-    look_at(get_global_mouse_position())
-    target_velocity = transform.x * Input.get_axis("down", "up") * speed
-    if target_velocity.length() > 0:
-        inertia = target_velocity
-    else:
-        if inertia.length() > max_inertia:
-            inertia = inertia.normalized() * max_inertia
-        inertia = inertia.lerp(Vector2.ZERO, deceleration)
+	if health <= 0:
+		return
+	look_at(get_global_mouse_position())
+	target_velocity = transform.x * speed
+	if Input.is_action_pressed("down"):
+		if inertia.length() > max_inertia:
+			inertia = inertia.normalized() * max_inertia
+	else:
+		if target_velocity.length() > 0:
+			inertia = target_velocity
+		else:
+			inertia = inertia.lerp(Vector2.ZERO, deceleration)
+	if Input.is_action_just_pressed("fire"):
+		shoot()
+
+func shoot():
+	var bullet = bullet_path.instantiate()
+	var offset = Vector2(speed / float(2), 0).rotated(get_rotation())
+	bullet.set_position(get_position() + offset)
+	bullet.set_rotation(get_rotation())
+	get_parent().add_child(bullet)
 
 func _process(_delta):
-    if health <= 0:
-        return
+	if health <= 0:
+		return
 
 func _physics_process(_delta):
-    get_input()
-    velocity = inertia
-    move_and_slide()
+	get_input()
+	velocity = inertia
+	move_and_slide()
