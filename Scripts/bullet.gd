e319679a7662f7d16c04a1fc1a5fc920cfde2447
Mizerski <mizerskimachado@hotmail.com>
2024-04-08 19:13:07 -0300

    removendo bala foda matadora de players e pcs

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,8 +1,3 @@
 extends CharacterBody2D
 
-var bulletVelocity = Vector2(1,0)
-var speed = 200
-
-func _physics_process(delta):
-	var collision_info = move_and_collide(bulletVelocity.normalized() * delta * speed)
 
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,34 +5,27 @@ extends CharacterBody2D
 @export var damage = 10
 @export var max_inertia = 20 
 @export var deceleration = 0.01 
+
 var target_velocity = Vector2()
 var inertia = Vector2()
-const bulletPath= preload("res://Models/bullet.tscn")
+
 func get_input():
-	if health <= 0:
-		return
-	look_at(get_global_mouse_position())
-	target_velocity = transform.x * Input.get_axis("down", "up") * speed
-	if target_velocity.length() > 0:
-		inertia = target_velocity
-	else:
-		if inertia.length() > max_inertia:
-			inertia = inertia.normalized() * max_inertia
-		inertia = inertia.lerp(Vector2.ZERO, deceleration)
-func shoot():
-	if health <= 0:
-		return
-	var bullet = bulletPath.instantiate()
-	get_parent().add_child(bullet)
-	bullet.position = $ShootPoint.global_position 
+    if health <= 0:
+        return
+    look_at(get_global_mouse_position())
+    target_velocity = transform.x * Input.get_axis("down", "up") * speed
+    if target_velocity.length() > 0:
+        inertia = target_velocity
+    else:
+        if inertia.length() > max_inertia:
+            inertia = inertia.normalized() * max_inertia
+        inertia = inertia.lerp(Vector2.ZERO, deceleration)
+
 func _process(_delta):
-	if health <= 0:
-		return
-	if Input.is_action_pressed("fire"):
-		shoot()
+    if health <= 0:
+        return
+
 func _physics_process(_delta):
-	get_input()
-	velocity = inertia
-	move_and_slide()
-
-
+    get_input()
+    velocity = inertia
+    move_and_slide()
